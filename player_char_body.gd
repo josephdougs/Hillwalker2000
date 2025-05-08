b20407a24d66953eef2ddbc4d3dcb60bed86df9a
Joey Smith <josephdougs@gmail.com>
2025-05-07 21:42:00 -0700

    Working staying on terrain!

--- player_char_body.gd
+++ player_char_body.gd
@@ -4,10 +4,29 @@ extends CharacterBody3D
 var myself: CharacterBody3D
 var myCamera: Camera3D
 var hTerrain
+var currentNormalY: float = 1
+var currentPositionY: float = self.global_position.y
 
 func _ready() -> void:
 	myCamera = $"./Camera3D"
 	hTerrain = $"../../HTerrain"
+	
+func _physics_process(delta):
+	var dict = get_ground_normal()
+	currentNormalY = dict["normal"].y
+	currentPositionY = dict["position"].y + 0.5
+	
+	self.global_position.y = currentPositionY
+	
+	
+func get_ground_normal():
+	var space_state = get_world_3d().direct_space_state
+	# use global coordinates, not local to node
+	var query = PhysicsRayQueryParameters3D.create(self.global_position, self.global_position - Vector3(0,1000,0))
+	var result = space_state.intersect_ray(query)
+	
+	return result
+
 
 func _process(delta: float) -> void:
 	var collision = move_and_collide(Vector3(-1,0,0) * delta)
--- playercontainer.gd
+++ playercontainer.gd
@@ -13,24 +13,8 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("switch"):
-		switchChars()
-		print(currentNormalY)
-		
-func _physics_process(delta):
-	currentNormalY = get_ground_normal()
+		switchChars() 
 	
-	
-func get_ground_normal():
-	var space_state = get_world_3d().direct_space_state
-	# use global coordinates, not local to node
-	var query = PhysicsRayQueryParameters3D.create(currentBody.global_position, currentBody.global_position - Vector3(0,1000,0))
-	var result = space_state.intersect_ray(query)
-	
-	if result.has("normal"):
-		return result["normal"].y
-	else:
-		# just hack it and return 1 for now in this case
-		return 1
 	
 		
 func switchChars():
